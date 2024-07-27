8efd6381e9ebb9fbae2a511e2543e296eefe44c0
kkanami <k3064782@gmail.com>
2024-07-27 14:55:35 +0900

    0727

--- diworks_keijiban/index.php
+++ diworks_keijiban/index.php
@@ -1,15 +1,14 @@
+<!doctype html>
+<html lang="ja">
 
- <!doctype html>
- <html lang="ja">
+<head>
+    <meta charset="utf-8">
+    <title>diworksblog 掲示板</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
 
- <head>
-     <meta charset="utf-8">
-     <title>diworksblog 掲示板</title>
-     <link rel="stylesheet" type="text/css" href="style.css">
- </head>
-
- <body>
-     <?php
+<body>
+    <?php
     //PDO
     mb_internal_encoding("utf8");
  $pdo=new PDO("mysql:dbname=lesson01;host=localhost;","root","");
@@ -18,60 +17,60 @@
   
     ?>
 
-     <header>
-         <img src="diblog_logo.jpg">
-         <div class="content">
-             <ul class="menu">
-                 <li>トップ</li>
-                 <li>プロフィール</li>
-                 <li>D.I.Blogについて</li>
-                 <li>登録フォーム</li>
-                 <li>問い合わせ</li>
-                 <li>その他</li>
-             </ul>
-         </div>
-     </header>
+    <header>
+        <img src="diblog_logo.jpg">
+        <div class="content">
+            <ul class="menu">
+                <li>トップ</li>
+                <li>プロフィール</li>
+                <li>D.I.Blogについて</li>
+                <li>登録フォーム</li>
+                <li>問い合わせ</li>
+                <li>その他</li>
+            </ul>
+        </div>
+    </header>
 
-     <main>
-         <div class="main">
-             <div class="left">
-                 <h1>プログラミングに役立つ掲示板</h1>
+    <main>
+        <div class="main">
+            <div class="left">
+                <h1>プログラミングに役立つ掲示板</h1>
 
-                 <div class="form">
-                     <h3>入力フォーム</h3>
-                     
-                     <form method="post" action="insert.php" required>
-                         <div>
-                             <label>ハンドルネーム</label>
-                             <br>
-                             <input type="text" class="text" size="35" name="handlename" required>
+                <div class="form">
+                    <h3>入力フォーム</h3>
 
-                         </div>
+                    <form method="post" action="insert.php">
+                        <div>
+                            <label>ハンドルネーム</label>
+                            <br>
+                            <input type="text" class="text" size="35" name="handlename">
 
-                         <div>
-                             <label>タイトル</label>
-                             <br>
-                             <input type="text" class="text" size="35" name="title" required>
+                        </div>
 
-                         </div>
+                        <div>
+                            <label>タイトル</label>
+                            <br>
+                            <input type="text" class="text" size="35" name="title">
 
-                         <div>
-                             <label>コメント</label>
-                             <br>
-                             <!--<textarea></textarea>の間に改行やインデントいれると謎のスペースがはいってしまうので注意-->
-                             <textarea cols="35" rows="7" name="comments" required></textarea>
-                         </div>
+                        </div>
 
-                         <div>
-                             <input type="submit" class="submit" value="投稿する">
-                         </div>
+                        <div>
+                            <label>コメント</label>
+                            <br>
+                            <!--<textarea></textarea>の間に改行やインデントいれると謎のスペースがはいってしまうので注意-->
+                            <textarea cols="35" rows="7" name="comments"></textarea>
+                        </div>
 
-                     </form>
+                        <div>
+                            <input type="submit" class="submit" value="投稿する">
+                        </div>
 
-                 </div>
-                 <br>
+                    </form>
 
-                 <?php
+                </div>
+                <br>
+
+                <?php
                   //投稿を表示させるrow…行 stmt…statementの略。声明 fetch…取ってくる
     while($row=$stmt->fetch()){
              echo'<div class="kiji">';
@@ -84,40 +83,40 @@
             }
               ?>
 
-             </div>
-             <div class="right">
-                 <h3>人気の記事</h3>
-                 <ul>
-                     <li><a href=""> PHPオススメ本</a></li>
-                     <li><a href="">PHP　MyAdminの使い方</a></li>
-                     <li><a href="">いま人気のエディタTops</a></li>
-                     <li><a href="">HTMLの基礎</a></li>
-                 </ul>
+            </div>
+            <div class="right">
+                <h3>人気の記事</h3>
+                <ul>
+                    <li><a href=""> PHPオススメ本</a></li>
+                    <li><a href="">PHP　MyAdminの使い方</a></li>
+                    <li><a href="">いま人気のエディタTops</a></li>
+                    <li><a href="">HTMLの基礎</a></li>
+                </ul>
 
-                 <h3>オススメリンク</h3>
-                 <ul>
-                     <li><a href=""> ﾃﾞｨｰｱｲﾜｰｸｽ株式会社</a></li>
-                     <li><a href="">XAMPPのダウンロード</a></li>
-                     <li> <a href="">Eclipseのダウンロード</a></li>
-                     <li> <a href="">Braketsのダウンロード</a></li>
-                 </ul>
+                <h3>オススメリンク</h3>
+                <ul>
+                    <li><a href=""> ﾃﾞｨｰｱｲﾜｰｸｽ株式会社</a></li>
+                    <li><a href="">XAMPPのダウンロード</a></li>
+                    <li> <a href="">Eclipseのダウンロード</a></li>
+                    <li> <a href="">Braketsのダウンロード</a></li>
+                </ul>
 
-                 <h3>カテゴリ</h3>
-                 <ul>
-                     <li> <a href=""> HTML</a></li>
-                     <li><a href=""> PHP</a></li>
-                     <li><a href=""> MySQL</a></li>
-                     <li><a href=""> JavaScript</a></li>
-                 </ul>
+                <h3>カテゴリ</h3>
+                <ul>
+                    <li> <a href=""> HTML</a></li>
+                    <li><a href=""> PHP</a></li>
+                    <li><a href=""> MySQL</a></li>
+                    <li><a href=""> JavaScript</a></li>
+                </ul>
 
-             </div>
+            </div>
 
-         </div>
-     </main>
+        </div>
+    </main>
 
-     <footer>
-         Copyright D.I.works| D.I.blog is the one which provides A to Z about programming
-     </footer>
- </body>
+    <footer>
+        Copyright D.I.works| D.I.blog is the one which provides A to Z about programming
+    </footer>
+</body>
 
- </html>
+</html>
--- diworks_keijiban/insert.php
+++ diworks_keijiban/insert.php
@@ -3,9 +3,15 @@
 mb_internal_encoding("utf8");
 //DB接続
 $pdo=new PDO("mysql:dbname=lesson01;host=localhost;","root","");
+if(empty($_POST['handlename']) || empty($_POST['title']) || empty($_POST['comments'])){}
+else{
 $pdo->exec("insert into diworks_keijiban(handlename,title,comments)
 values('".$_POST['handlename']."','".$_POST['title']."','".$_POST['comments']."');");
+    }
 //リダイレクトの処理
 header("Location:http://localhost/diworks_keijiban/index.php");
 ?>
 
+if(empty($_POST['check_data'])){
+    echo "未入力です。";
+}
